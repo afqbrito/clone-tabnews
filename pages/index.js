34b5e20927fccd04c20ef1331fe361a0e3dd1b70
Adriano Fabio Brito <adriano.fabio@gmail.com>
2023-09-20 23:53:38 +0000

    mensagem com emojis

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ooooi! Se você chegou aqui, então dê uma risadinha!</h1>
+    return <h1>Ooooi! Se você chegou aqui, então dê uma risadinha! 😉😂</h1>
 }
 
 export default Home;
